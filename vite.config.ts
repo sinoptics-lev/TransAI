3db87d8e05ac75d29de82c66c10db0840bdd1ee4
sinoptics-lev <sinoptics@gmail.com>
2026-07-10 12:11:06 +0300

    TransAI v2.5.5 — Configs and README

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
+})
